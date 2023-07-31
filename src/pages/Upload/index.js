4106dab94ed82d8a2a12f32c062b23c743873f01
Hoanpk <phunghoan13@gmail.com>
2023-07-31 10:56:33 +0700

    first commit

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -1,14 +1,5 @@
-import Header from '~/components/Layout/DefaultLayout/Header';
-
 function Upload() {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <h2>Upload</h2>
-            </div>
-        </div>
-    );
+    return <h2>Upload</h2>;
 }
 
 export default Upload;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,19 @@
+// Layout
+import { HeaderOnly } from '~/components/Layout';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 // public routes
 const publicRouter = [
     { path: '/', component: Home },
     { path: '/Following', component: Following },
     { path: '/Profile', component: Profile },
-    { path: '/Upload', component: Upload, Layout: null },
+    { path: '/Search', component: Search, layout: null },
+    { path: '/Upload', component: Upload, layout: HeaderOnly },
 ];
 
 const privateRouter = [];
